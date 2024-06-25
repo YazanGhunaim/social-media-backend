d2a0c67e8b4054e59490cc48d360c6d671c3f5bc
yazan ghunaim <yazanghunaim07@gmail.com>
2024-06-25 08:34:31 +0200

    added healthcheck route

--- app/main.py
+++ app/main.py
@@ -21,3 +21,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get('/healthz')
+def healthcheck():
+    return "Yes!"
